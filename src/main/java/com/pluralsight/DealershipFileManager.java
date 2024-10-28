320018573d3e17cf56ebd190564045cc0a9b6c39
Taseem Molique <taseenm100@gmail.com>
2024-10-28 13:31:42 -0400

    half of User Interface

--- src/main/java/com/pluralsight/DealershipFileManager.java
+++ src/main/java/com/pluralsight/DealershipFileManager.java
@@ -1,4 +1,49 @@
 package com.pluralsight;
 
+import java.io.*;
+
 public class DealershipFileManager {
+    private static final String filePath = "Vehicles.csv";
+
+    public static Dealership loadDealership() {
+        try (BufferedReader bf = new BufferedReader(new FileReader(filePath))){
+            String[] dealershipInfo = bf.readLine().split("\\|");
+            Dealership dealership = new Dealership(dealershipInfo[0], dealershipInfo[1], dealershipInfo[2]);
+            String line;
+            while ((line = bf.readLine()) != null) {
+                String[] vehicleData = line.split("\\|");
+                Vehicle vehicle = new Vehicle(
+                        Integer.parseInt(vehicleData[0]),
+                        Integer.parseInt(vehicleData[1]),
+                        vehicleData[2],
+                        vehicleData[3],
+                        vehicleData[4],
+                        vehicleData[5],
+                        Integer.parseInt(vehicleData[6]),
+                        Double.parseDouble(vehicleData[7])
+                );
+                dealership.addVehicle(vehicle);
+            }
+            return dealership;
+        } catch (FileNotFoundException fnfe){
+            System.out.println("File now foundL " + filePath);
+            return new Dealership("Default Dealership", "N/A", "N/A");
+        } catch (Exception e) {
+            System.out.println("Error loading dealership data." + e.getMessage());
+            return null;
+        }
+    }
+
+    public  static void saveDealership(Dealership dealership){
+        try {
+            FileWriter writer = new FileWriter(filePath);
+            writer.write(dealership.getName() + "|" + dealership.getAddress() + "|" + dealership.getPhone() + "\n");
+            for (Vehicle vehicle : dealership.listAllVehicles()){
+                writer.write(vehicle.toString() + "\n");
+            }
+        } catch (IOException ioex) {
+            System.out.println("Error saving dealership data.");
+        }
+    }
+
 }
--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -1,4 +1,79 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class UserInterface {
+    private final Dealership dealership;
+    private final Scanner scanner;
+
+    public UserInterface(Dealership dealership) {
+        this.dealership = dealership;
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void start() {
+        while (true) {
+            System.out.println("1. Find vehicles within a price range");
+            System.out.println("2. Find vehicles by make/model");
+            System.out.println("3. Find vehicles by year range");
+            System.out.println("4. Find vehicles by color");
+            System.out.println("5. Find vehicles by mileage range");
+            System.out.println("6. Find vehicles by type");
+            System.out.println("7. List all vehicles");
+            System.out.println("8. Add a vehicle");
+            System.out.println("9. Remove a vehicle");
+            System.out.println("0. Quit");
+            System.out.println("Please select an option");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice){
+
+            }
+
+        }
+    }
+
+    private void findByPriceRange() {
+        System.out.println("Enter minimum price: ");
+        double min = scanner.nextDouble();
+        System.out.println("Enter maximum price: ");
+        double max = scanner.nextDouble();
+        Vehicle[] vehicles = dealership.findByPriceRange(min, max);
+        displayVehicles(vehicles);
+    }
+
+    private void findByMakeModel() {
+        System.out.println("Enter make: ");
+        String make = scanner.nextLine();
+        System.out.println("Enter model: ");
+        String model = scanner.nextLine();
+        Vehicle[] vehicles = dealership.findByMakeModel(make, model);
+        displayVehicles(vehicles);
+    }
+
+    private void findByYearRange() {
+        System.out.println("Enter start year: ");
+        int startYear = scanner.nextInt();
+        System.out.println("Enter end year: ");
+        int endYear = scanner.nextInt();
+        Vehicle[] vehicles = dealership.findByYearRange(startYear, endYear);
+        displayVehicles(vehicles);
+    }
+
+    
+
+    private void displayVehicles(Vehicle[] vehicles){
+        if (vehicles.length == 0 || vehicles[0] == null){
+            System.out.println("No vehicles found.");
+        } else {
+            for (Vehicle v : vehicles) {
+                if (v != null) {
+                    System.out.println(v);
+                }
+            }
+        }
+    }
+
 }
